578d7f4db1f3127a0a9fcef8f0abd4c16cd175b6
Hayden Wang <hydnwang@gmail.com>
2019-11-26 14:35:51 -0800

    merge feature/auth

--- schema.sql
+++ schema.sql
@@ -7,3 +7,15 @@ CREATE TABLE IF NOT EXISTS registration (
     meeting_time VARCHAR(100) NOT NULL,
     PRIMARY KEY (course_id, user_id)
 );
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT '',
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `major` varchar(100) COLLATE utf8mb4_general_ci DEFAULT '',
+  `interests` text COLLATE utf8mb4_general_ci,
+  `privacy` int(10) unsigned DEFAULT '0',
+  `role` int(10) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`)
+); 
