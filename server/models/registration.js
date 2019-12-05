c8871bc78b042bdaf683dc920bf539acac7fadf0
peiyuan chen <peiyuac1@uci.edu>
2019-12-04 18:01:09 -0800

    check before db injection

--- server/models/registration.js
+++ server/models/registration.js
@@ -4,11 +4,18 @@ const mysql = require('mysql')
 
 exports.addCourse=function(course_id, user_id, course_title, course_type, course_time){
     var date = new Date();
-    var query = "INSERT INTO registration (course_id,user_id, reg_time,course_title, course_type,meeting_time) VALUES (?);";
-    var value=[course_id,user_id,date.getTime(),course_title,course_type,course_time];
-    sql.query(query, [value],function(err,result){
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
+    var if_take=false;
+    selectCourse(course_id, user_id, (Data) => {
+        if (Array.isArray(Data) && Data.length) {
+          console.log("you are already in the course");
+        } else {
+            var query = "INSERT INTO registration (course_id,user_id, reg_time,course_title, course_type,meeting_time) VALUES (?);";
+            var value=[course_id,user_id,date.getTime(),course_title,course_type,course_time];
+            sql.query(query, [value],function(err,result){
+                if (err) throw err;
+                console.log("Number of records inserted: " + result.affectedRows);
+            });
+        }
     });
 }
 exports.dropCourse=function(course_id, user_id){
@@ -21,3 +28,12 @@ exports.dropCourse=function(course_id, user_id){
         console.log("Number of records delete: " + result.affectedRows);
     });
 };
+
+selectCourse = function(course_id, user_id,callback){
+    let query = 'SELECT * FROM registration WHERE ??=? AND ??=?'
+    let param = ['course_id', course_id, 'user_id', user_id]
+    sql.query(query, param, function (error, result) {
+    if (error) { console.log('[Error] ', error) }
+    callback(result);
+  });
+}
