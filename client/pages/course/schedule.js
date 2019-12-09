4c8cb3b0c8f6e10935a613d2ac59862dac36a746
peiyuan chen <peiyuac1@uci.edu>
2019-12-08 23:44:19 -0800

    fix sunday no data on schedule

--- client/pages/course/schedule.js
+++ client/pages/course/schedule.js
@@ -30,16 +30,18 @@ const getWeekDay = function(){
   var currentDate = new Date(dateOfToday);
   var timesStamp = currentDate.getTime();
   var currenDay = currentDate.getDay();
-  console.log("今天是："+currenDay);
+  // console.log("今天是："+currenDay);
   var dates = [];
   for (var i = 0; i < 7; i++) {
-      dates.push(new Date(timesStamp + 24 * 60 * 60 * 1000 * (i - (currenDay + 6) % 7)));
+      dates.push(new Date(timesStamp + 24 * 60 * 60 * 1000 * ((currenDay + i) % 7)));
   }
-  // console.log("dates:"+dates);
+  console.log("dates:"+dates);
   return dates;
 };
 
 const parseTime=function(data){
+  // console.log("data:");
+  // console.log(data);
   var dates=getWeekDay();
   var res=[];
   data.forEach(item=>{
@@ -64,28 +66,30 @@ const parseTime=function(data){
       var end_hour=0;
       var end_min = 0;
       if(group[0]=="M"){
-        day = dates[0].getDate();
-        month = dates[0].getMonth();
-        year = dates[0].getYear();
-      }else if(group[0]=="Tu"){
         day = dates[1].getDate();
         month = dates[1].getMonth();
         year = dates[1].getYear();
-      }else if(group[0]=="W"){
+      }else if(group[0]=="Tu"){
         day = dates[2].getDate();
         month = dates[2].getMonth();
         year = dates[2].getYear();
-      }else if(group[0]=="Th"){
+      }else if(group[0]=="W"){
         day = dates[3].getDate();
         month = dates[3].getMonth();
         year = dates[3].getYear();
-      }else if(group[0]=="F"){
+      }else if(group[0]=="Th"){
         day = dates[4].getDate();
         month = dates[4].getMonth();
         year = dates[4].getYear();
+      }else if(group[0]=="F"){
+        day = dates[5].getDate();
+        month = dates[5].getMonth();
+        year = dates[5].getYear();
       }
       var hour_slot = group[1].split(/[-:]/);
-      if(group[1].charAt(group[1].length-1)=="p"){
+      // console.log(hour_slot);
+      if(group[1].charAt(group[1].length-1)=="p"&&parseInt(hour_slot[0])&&parseInt(hour_slot[2])<12){
+        // console.log("in +12")
         start_hour = parseInt(hour_slot[0])+12;
         start_min = parseInt(hour_slot[1]);
         end_hour = parseInt(hour_slot[2])+12;
@@ -124,13 +128,13 @@ class Schedule extends React.PureComponent {
 
   setTerm = (term) => {
     this.setState({ term: term });
-    console.log('in setTerm:' + term);
+    // console.log('in setTerm:' + term);
     this.getCourse(term);
   };
 
   getCourse = (term)=>{
-    console.log("in gerCourse:"+ term);
-    console.log("in gerCourse this.state.term:"+this.state.term);
+    // console.log("in gerCourse:"+ term);
+    // console.log("in gerCourse this.state.term:"+this.state.term);
     var u_id = this.props.query.u_id || this.props.token;
     if (u_id != undefined) {
       fetch('/api/users/' + u_id)
@@ -159,6 +163,7 @@ class Schedule extends React.PureComponent {
   render()
   {
     const { data, currentDate, userName } = this.state;
+    // console.log(this.state.data);
     return (
       <Layout title="Schedule" loginStatus={this.props.loginStatus} background={background}>
         <Container maxWidth = 'lg' style={{ flex: 1 }}>
